38d9e26ab87cd401db13fdf35aed983dfc8298cd
Dion Chua <dion@ticker.ninja>
2022-01-20 11:59:31 +0800

    Initial Commit (Frontend)

--- client/src/pages/RootPage/index.js
+++ client/src/pages/RootPage/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 export default class RootPage extends Component {
   render() {
     return (

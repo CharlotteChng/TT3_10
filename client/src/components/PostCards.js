b13621adb862f29ddf4d10aa6026d42fd066a66b
Dion Chua <dion@ticker.ninja>
2022-01-20 17:03:36 +0800

    Update post card design.

--- client/src/components/PostCards.js
+++ client/src/components/PostCards.js
@@ -1,5 +1,5 @@
-import { Image, Typography } from "antd";
-import { CaretUpOutlined, CaretDownOutlined } from "@ant-design/icons";
+import { Avatar, Badge, Button, Col, Image, Row, Typography } from "antd";
+import { CaretUpOutlined, CaretDownOutlined, LikeOutlined, ShareAltOutlined } from "@ant-design/icons";
 function PostCards({ ...props }) {
   var { postID, postTitle, postDescription, postImage } = props;
   const { Title, Text } = Typography;
@@ -12,7 +12,7 @@ function PostCards({ ...props }) {
           <div className="pc-r1-lc">
             <Image
               className="pc-img"
-              width={100}
+              width={64}
               src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
               borderRadius="50%"
             />
@@ -23,18 +23,21 @@ function PostCards({ ...props }) {
                 <Title level={3} style={{ marginLeft: "10px" }}>
                   {postTitle}
                 </Title>
+                <Text level={5} style={{ marginLeft: "10px" }}>
+                  User: jackHamilton
+                </Text>
               </div>
-              <div className="pc-icons">
-                <CaretDownOutlined style={{ iconSize: "24px" }} />
-                <CaretUpOutlined />
-              </div>
+              {/* <div className="pc-icons">
+                <CaretUpOutlined style={{ color: 'green', fontSize: "32px" }} />
+                <Title level={5}>0</Title>
+                <CaretDownOutlined style={{ color: 'red', fontSize: "32px" }} />
+              </div> */}
             </div>
-
-            <Text level={5} style={{ marginLeft: "10px" }}>
-              User: jackHamilton
-            </Text>
             {/* <div className="pc-title">{postTitle}</div> */}
           </div>
+        </div>
+        <div className="pc-r3">
+          <div className="pc-desc">{postDescription}</div>
         </div>
         <div className="pc-r2">
           <div className="pc-img">
@@ -42,15 +45,25 @@ function PostCards({ ...props }) {
             <Image
               src={postImage}
               alt="loading..."
-              style={{ width: "200px", height: "200px" }}
+              style={{ width: "100%" }}
             />
             {/* <Video src={"https://i.redd.it/600fjw70hqb81.jpg"} alt="new" /> */}
           </div>
         </div>
-        <div className="pc-r3">
-          <div className="pc-desc">{postDescription}</div>
-        </div>
       </div>
+
+      <Badge count={3} style={{ marginTop: 10, marginBottom: 20 }}>
+        <Avatar size={32} icon={<LikeOutlined />} />
+      </Badge>
+
+      <Row style={{ textAlign: 'center' }}>
+        <Col span={12}>
+          <Button style={{ width: '100%' }}><LikeOutlined />Like</Button>
+        </Col>
+        <Col span={12}>
+          <Button style={{ width: '100%' }}><ShareAltOutlined />Share</Button>
+        </Col>
+      </Row>
     </div>
   );
 }
